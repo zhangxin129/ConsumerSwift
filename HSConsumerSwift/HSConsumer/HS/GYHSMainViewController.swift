7efac617df54e12495b7a40c2547e90546109181
zhangx <zhangx@gyist.com>
2016-11-14 21:03:49 +0800

    提交代码

--- HSConsumerSwift/HSConsumer/HS/GYHSMainViewController.swift
+++ HSConsumerSwift/HSConsumer/HS/GYHSMainViewController.swift
@@ -9,12 +9,66 @@
 import UIKit
 
 class GYHSMainViewController: UIViewController {
-
+    
+    override func viewWillAppear(animated: Bool) {
+        
+            super.viewWillAppear(animated)
+        
+        self.navigationController?.navigationBar.hidden = true
+        
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+        
+        self.navigationController?.navigationBar.hidden = false
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.view.backgroundColor = UIColor .blueColor()
- 
+        self.view.backgroundColor = UIColor .whiteColor()
+        
+        let headView : UIView = UIView(frame : CGRectMake(0,0,self.view.frame.size.width,150))
+        
+        headView.backgroundColor = UIColor .blueColor()
+        
+        self.view .addSubview(headView)
+        
+        
+        let imgArray : Array<String> = ["gyhs_sweep_code_pay","gyhs_payment_code","gyhs_integral_code","gyhs _exchange_hsb"]
+        
+        let titleArray : Array<String> = ["扫码付","支付码","积分码","兑换互生币"]
+        
+        let labelArray : Array<String> = ["扫商家收款码","互生币支付","互生卡二维码","用于互生币支付"]
+        
+        let distance_x : CGFloat = (self.view.frame.size.width - 240 - 40)/3
+        
+        
+        for i in 0...3 {
+            
+            let mainBtn : UIButton = UIButton(type : UIButtonType.Custom)
+            
+            mainBtn.frame = CGRectMake(20 + CGFloat(i) * (60 + distance_x), 60, 60, 60)
+            
+            mainBtn .setImage(UIImage (named: imgArray[i]), forState: UIControlState.Normal)
+            
+            mainBtn .setTitle(titleArray[i], forState: UIControlState.Normal)
+            
+            mainBtn .setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            
+            mainBtn .setTitleColor(UIColor .yellowColor(), forState: UIControlState.Selected)
+            
+            self.view .addSubview(mainBtn)
+            
+            
+        }
+        
+        
+        
+        
     }
    
     override func didReceiveMemoryWarning() {
