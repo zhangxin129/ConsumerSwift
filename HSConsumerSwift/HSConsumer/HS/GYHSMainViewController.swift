58721f4c09d0f2a29ece6b3977bc6450ead0dc16
zhangx <zhangx@gyist.com>
2016-11-16 20:58:28 +0800

    提交代码

--- HSConsumerSwift/HSConsumer/HS/GYHSMainViewController.swift
+++ HSConsumerSwift/HSConsumer/HS/GYHSMainViewController.swift
@@ -44,25 +44,48 @@ class GYHSMainViewController: UIViewController {
         
         let labelArray : Array<String> = ["扫商家收款码","互生币支付","互生卡二维码","用于互生币支付"]
         
-        let distance_x : CGFloat = (self.view.frame.size.width - 240 - 40)/3
+        let distance_x : CGFloat = (self.view.frame.size.width - 160 - 40)/3
+        
+        let dis_x : CGFloat = (self.view.frame.size.width - 240 - 20)/3
         
         
         for i in 0...3 {
             
             let mainBtn : UIButton = UIButton(type : UIButtonType.Custom)
             
-            mainBtn.frame = CGRectMake(20 + CGFloat(i) * (60 + distance_x), 60, 60, 60)
+            mainBtn.frame = CGRectMake(20 + CGFloat(i) * (40 + distance_x), 44, 50, 50)
             
-            mainBtn .setImage(UIImage (named: imgArray[i]), forState: UIControlState.Normal)
+            mainBtn .setBackgroundImage(UIImage (named: imgArray[i]), forState: UIControlState.Normal)
             
             mainBtn .setTitle(titleArray[i], forState: UIControlState.Normal)
+            
+            mainBtn.imageEdgeInsets = UIEdgeInsetsMake(-20, 0, 0, 0)
+            
+            mainBtn.titleEdgeInsets = UIEdgeInsetsMake(0, 0, -70, 0)
+            
+            mainBtn.titleLabel?.font = UIFont .systemFontOfSize(10.0)
             
             mainBtn .setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
             
             mainBtn .setTitleColor(UIColor .yellowColor(), forState: UIControlState.Selected)
             
+            mainBtn .addTarget(self, action:#selector(GYHSMainViewController.btnClick(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+            
+            mainBtn.tag = i + 100
+            
             self.view .addSubview(mainBtn)
             
+            let mainLabel : UILabel = UILabel(frame : CGRectMake(15 + CGFloat(i) * (60 + dis_x), CGRectGetMaxY(mainBtn.frame)+10, 60, 40))
+            
+            mainLabel.text = labelArray[i]
+            
+            mainLabel.textColor = UIColor .whiteColor()
+            
+            mainLabel.textAlignment = NSTextAlignment.Center
+            
+            mainLabel.font = UIFont .systemFontOfSize(10.0)
+            
+            self.view .addSubview(mainLabel)
             
         }
         
@@ -70,12 +93,47 @@ class GYHSMainViewController: UIViewController {
         
         
     }
-   
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
+    func btnClick(sender : UIButton){
+    
+        
+        for i in 0...3 {
+            
+            let  btn : UIButton =  self.view .viewWithTag(100+i) as! UIButton
+            
+            btn.selected = false
+            
+        }
+        
+        let btn =  self.view .viewWithTag(sender.tag) as? UIButton
+        
+        btn!.selected = true
+        
+        switch sender.tag {
+        case 100:
+            
+            print(sender.tag)
+            
+        case 101:
+            
+             print(sender.tag)
+            
+        case 102:
+            
+             print(sender.tag)
+            
+        case 103:
+            
+             print(sender.tag)
+            
+        default:
+             print("others")
+            
+        }
+        
+
+        
+    }
 
     /*
     // MARK: - Navigation
